c694f7c4417a6fc6be5ea21fe1a8fcaad38cd2c1
OlegKKK <kramo@ukr.net>
2016-10-06 11:22:14 +0300

    Add window

--- src/game/tetris/Tetris.java
+++ src/game/tetris/Tetris.java
@@ -1,4 +1,28 @@
 package game.tetris;
 
-public class Tetris {
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class Tetris extends JPanel{
+
+    static Tetris tetris = new Tetris();
+    static JFrame window = new JFrame();
+
+    Tetris() {
+
+        super();
+        setBackground(Color.DARK_GRAY);
+        setLayout(null);
+
+    }
+
+    public static void main(String [] args) {
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.add(tetris);
+        window.setSize(600, 600);
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setVisible(true);
+    }
 }

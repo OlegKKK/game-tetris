1921a1696a754bca8e3756b6216bc2e929a9050d
OlegKKK <kramo@ukr.net>
2016-10-06 12:46:59 +0300

    Add blocks

---
+++ src/game/tetris/Timer.java
@@ -0,0 +1,19 @@
+package game.tetris;
+
+public class Timer {
+    final static boolean[][][] TIMER =
+            {
+                    {
+                            {false, false, false, false},
+                            {true, true, true, false},
+                            {false, false, true, false},
+                            {false, false, false, false},
+                    },
+                    {
+                            {false, false, false, false},
+                            {true, true, true, false},
+                            {false, true, false, false},
+                            {false, false, false, false},
+            }
+            };
+}

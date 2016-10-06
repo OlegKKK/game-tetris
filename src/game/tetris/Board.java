d6291859b716137d31607f6d48978ad9e977db4d
OlegKKK <kramo@ukr.net>
2016-10-06 12:11:52 +0300

    Add colors

--- src/game/tetris/Board.java
+++ src/game/tetris/Board.java
@@ -1,10 +1,13 @@
 package game.tetris;
 
+import java.awt.*;
+
 public class Board extends ACanvas{
 
     final static short SIZE = 25;
     final static short WIDTH = SIZE * 10;
     final static short HEIGHT = SIZE * 20;
+    final Color [] COLORS = {Color.BLACK, Color.RED, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.ORANGE, Color.YELLOW, Color.WHITE};
 
     Board() {
         super(WIDTH, HEIGHT);
--- src/game/tetris/Tetris.java
+++ src/game/tetris/Tetris.java
@@ -4,16 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 
 @SuppressWarnings("serial")
-public class Tetris extends JPanel{
+public class Tetris extends JPanel implements Runnable{
 
     static Tetris tetris = new Tetris();
     static JFrame window = new JFrame();
+    static Thread thread = new Thread(tetris);
     static Board board = new Board();
+
+    boolean start = false;
+    short scr = 50;
 
     Tetris() {
         super();
         setBackground(Color.DARK_GRAY);
         setLayout(null);
+        start = true;
     }
 
     public static void main(String [] args) {
@@ -25,5 +30,21 @@ public class Tetris extends JPanel{
         board.setLocation(10, 10);
         tetris.add(board);
         window.setVisible(true);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        long wait, startTime, loopTime;
+        while (start){
+            startTime = System.nanoTime();
+            board.run();
+            loopTime = startTime - System.nanoTime();
+            wait = scr - loopTime / 1000000;
+            if (wait<=0) wait = 3;
+            try {thread.sleep(scr);} catch (InterruptedException e) {e.printStackTrace();}
+            System.out.println(scr+" > "+wait);
+        }
+
     }
 }

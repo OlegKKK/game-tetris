c750f9ee1d65a41ce0043b845d18cb4f72e5e442
OlegKKK <kramo@ukr.net>
2016-10-09 11:45:04 +0300

    Fix window for next figure

--- src/game/tetris/Next.java
+++ src/game/tetris/Next.java
@@ -1,13 +1,10 @@
 package game.tetris;
 
-import java.awt.*;
-
 @SuppressWarnings("serial")
 public class Next extends ACanvas{
 
     Next() {
         super((byte) 100, (byte)100);
-        setBackground(Color.CYAN);
     }
 
     @Override

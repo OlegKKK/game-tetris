23e6c532acc52395f83ae80c46372ea384431171
OlegKKK <kramo@ukr.net>
2016-10-09 11:43:47 +0300

    Added window for next figure

---
+++ src/game/tetris/Next.java
@@ -0,0 +1,17 @@
+package game.tetris;
+
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class Next extends ACanvas{
+
+    Next() {
+        super((byte) 100, (byte)100);
+        setBackground(Color.CYAN);
+    }
+
+    @Override
+    public void drawImage() {
+
+    }
+}
